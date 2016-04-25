905ee873706045997a111bfb1b8ae0e4d7388576
Sergio <sergio.pena@abiquo.com>
2016-04-25 10:23:09 +0200

    Update version

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs and configures kairosdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url	 'https://github.com/sergiopena/kairosdb-cookbook'
 issues_url	 'https://github.com/sergiopena/kairosdb-cookbook/issues'
-version          '0.1.0'
+version          '0.1.1'
